69912de612a0c06b144c82c5f19538b0b05c926b
Dennis Cope <coped@tutanota.com>
2019-09-28 21:33:46 -0700

    Change site stying and add image content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def full_title(title)
-        base_title = "Trike World Ltd."
+        base_title = "Trike World Limited"
         if title.empty?
             base_title
         else
