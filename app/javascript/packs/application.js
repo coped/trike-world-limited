dc38473755e936d8e792b8a3a58ad864c6f877bb
Dennis Cope <coped@tutanota.com>
2019-09-28 22:15:59 -0700

    Add temp fix for mobile navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
-require("packs/navbar-toggle.js")
+// require("packs/navbar-toggle.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
