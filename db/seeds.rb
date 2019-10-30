1bedfe8f182c4c4aa208dee9435a449c8a22d13d
Dennis Cope <coped@tutanota.com>
2019-10-30 15:19:55 -0700

    Add seeds to development db

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+User.create!(name:  "Example User",
+             email: "example@user.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+User.create!(name:  "Another User",
+             email: "another@user.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
