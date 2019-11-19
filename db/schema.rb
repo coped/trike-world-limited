9320167d784e6561cd0b4dc83b25e30f5fc46700
Dennis Cope <coped@tutanota.com>
2019-11-19 14:25:23 -0700

    Change author_id column name to user_id on posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_025159) do
+ActiveRecord::Schema.define(version: 2019_11_19_212125) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.integer "author_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
