354ed93f4751e0612cfabb69885656ea2494009c
Dennis Cope <dennisaaroncope@gmail.com>
2019-12-12 23:51:07 -0700

    Add foreign key to user_id column of posts table

---
+++ db/migrate/20191213064632_add_foreign_key_to_posts_user_id.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToPostsUserId < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :posts, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_004326) do
+ActiveRecord::Schema.define(version: 2019_12_13_064632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,4 +35,5 @@ ActiveRecord::Schema.define(version: 2019_12_11_004326) do
     t.string "road_name"
   end
 
+  add_foreign_key "posts", "users"
 end
